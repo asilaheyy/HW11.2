2db3b7fe41810fb92ad36a0184da7b2c07a14b87
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-09-22 16:29:58 +0300

    Сделано ДЗ 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    // ДЗ 2.
+    public static void OS(int clientOS, int deviceYear){
+
+        if (deviceYear<2015 && clientOS == 0){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }
+        if(deviceYear>=2015 && clientOS == 0){
+            System.out.println("Установите приложение для iOS по ссылке.");
+        }
+        if (deviceYear<2015 && clientOS == 1){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+        if(deviceYear>=2015 && clientOS == 1){
+            System.out.println("Установите приложение для Android по ссылке.");
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      int clientOS = 1;
+      int deviceYear = 2014;
+        OS(clientOS,deviceYear);
     }
 }
